0c5c0b02655be0b8f9f90f5a9317cfcee01d978f
tanvip1234 <38500698+tanvip1234@users.noreply.github.com>
2022-12-10 17:57:41 +0700

    elv

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -13,25 +13,56 @@ import { useState } from "react";
 import Paginate from "../single-dom/paginate/Paginate";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { products,setProducts } = useContext(ProductsContext);
 
   const [categories, setCategories] = React.useState("");
   const [price, setPrice] = React.useState("");
   //paginate
   const [currentPage, setCurrentPage] = useState(1);
-  const [postPerPage] = useState(2);
+  const [postPerPage,setPostPerPage] = useState(2);
+  const [productFilter,setProductFilter] = useState([])
   const handleChange = (event) => {
     setCategories(event.target.value);
+    const filterItemSelected = event.target.value
+    if(filterItemSelected === "Men Shoes"){
+      const filterItemProduct = products.filter(item => item.Categories === "Men Shoes")
+      setProductFilter(filterItemProduct)  
+    }
+    if(filterItemSelected === "Slides"){
+      const filterItemProduct = products.filter(item => item.Categories === "Slides")
+      setProductFilter(filterItemProduct)  
+    }
+    if(filterItemSelected === "Kid originals"){
+      const filterItemProduct = products.filter(item => item.Categories === "Kid originals")
+      setProductFilter(filterItemProduct)  
+    }
+    if(filterItemSelected === "All"){
+      const filterItemProduct = products
+      setProductFilter(filterItemProduct)  
+    }
   };
   const handleChangePrice = (event) => {
     setPrice(event.target.value);
+    
   };
+  const handleSearch = (event) => {
+    const userType = event.target.value;
+
+    if (userType.length > 0) {
+      const filterSearch = products.filter((item) => {
+        return item.ProductName.toLowerCase().includes(userType.toLowerCase());
+      });
+
+      setProductFilter(filterSearch);
+    }
+  }
 
   //paginate
   const lastPostIndex = postPerPage * currentPage;
   const firstPostIndex = lastPostIndex - postPerPage;
-  const currentPost = products.slice(firstPostIndex, lastPostIndex);
+  const filterProductsPaginate = productFilter.slice(firstPostIndex, lastPostIndex);
   // setProducts(currentPost)
+
   return (
     <>
       <div className="my-6 px-8 flex gap-4 items-center flex-wrap">
@@ -42,7 +73,7 @@ const Shop = () => {
                 Filter by Categories
               </InputLabel>
               <Select
-                defaultValue="Men Shoes"
+              
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
                 value={categories}
@@ -50,8 +81,10 @@ const Shop = () => {
                 onChange={handleChange}
                 inputProps={{ MenuProps: { disableScrollLock: true } }}
               >
+                <MenuItem value={'All'}>All</MenuItem>
+
                 <MenuItem value={"Men Shoes"}>Men Shoes</MenuItem>
-                <MenuItem value={"Kids originals"}>Kids originals</MenuItem>
+                <MenuItem value={"Kid originals"}>Kid originals</MenuItem>
                 <MenuItem value={"Slides"}>Slides</MenuItem>
               </Select>
             </FormControl>
@@ -84,15 +117,16 @@ const Shop = () => {
           label="Type to search"
           variant="outlined"
           inputProps={{ MenuProps: { disableScrollLock: true } }}
+          onChange={handleSearch}
         />
       </div>
    <>
         <div className="product-grid grid lg:grid-cols-4 lg:gap-2 md:grid-cols-2 grid-cols-1 h-full w-full px-6">
-          {currentPost.map((item) => {
+          {productFilter &&  filterProductsPaginate.map((item) => {
             return (
-              <>
+              
                 <ProductCard item={item} key={item.ID} />
-              </>
+              
             );
           })}
         </div>
